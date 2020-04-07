033dcddd4403588e46842d6072180ee279542823
Max Lamb <lordmaxlamb@gmail.com>
2020-04-06 17:29:36 -0700

    pretty much finished

--- brokenInstruments.js
+++ brokenInstruments.js
@@ -1,80 +1,70 @@
-// import instruments from 
 
-// const list = document.getElementById('instruments');
-
-
-
-// for (let i = 0; i < instruments.length; i++) {
-//     const instrument = instruments[i];
-//     const element = renderInstruments(instrument);
-//     list.appendChild(element);
-// }
 
 const trumpet = {
-    id: 'trumpet',
+    id: 1,
     name: 'Rusty trumpet',
     category: 'brass',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftimelinemusic.com%2Fproduct%2Fnew-rental-contract-for-trumpet-introductory-offer%2F&psig=AOvVaw0Il61_G7nqW4Te49CtBeXo&ust=1586296998394000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDykofn1OgCFQAAAAAdAAAAABAF',
+    image: 'https://az58332.vo.msecnd.net/e88dd2e9fff747f090c792316c22131c/Images/Products62075-1200x1200-1649647.jpg',  
     value: '1',
-    description: 'Sort of works. Might\'ve looked like the trumpet in the picture.',
+    description: 'Sort of works. Might\'ve looked like the trumpet in the picture at one point.',
     price: 5
 };
 
 const drumkit = {
-    id: 'drums',
+    id: 2,
     name: 'Destroyed drums',
     category: 'drums',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.zzounds.com%2Fitem--DRUPDCM2217&psig=AOvVaw0DBb1_qnm3NRJWdtT6tJhJ&ust=1586297535286000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDemfro1OgCFQAAAAAdAAAAABAE',
+    image: 'https://s3.amazonaws.com/images.static.steveweissmusic.com/products/images/uploads/1136796_33909_popup.jpg',
     value: '2',
     description: 'Ruined beyond repair.  Basically just garbage. Not the same drums in the picture.',
     price: 10
 };
 
 const guitar = {
-    id: 'guitar',
+    id: 3,
     name: 'Halved guitar',
     category: 'strings',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gear4music.com%2Fus%2Fen%2FElectric_Guitars%2FTypes&psig=AOvVaw1t9JZ_USCu7Wp5OptxZWek&ust=1586297603288000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPj7zprp1OgCFQAAAAAdAAAAABAG',
+    image: 'https://d1aeri3ty3izns.cloudfront.net/media/25/251309/1200/preview_2.jpg',
     value: '3',
     description: 'Definitely was smashed at the end of a concert.  Not the same guitar as the guitar in the picture.',
     price: 8
 };
 
 const bass = {
-    id: 'bass',
+    id: 4,
     name: 'Drowned bass',
     category: 'bass',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Freverb.com%2Fp%2Fsquier-vintage-modified-jazz-bass-70s&psig=AOvVaw2TG5qKDiD1E3F_BG4uigKO&ust=1586297639424000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDn6Kvp1OgCFQAAAAAdAAAAABAK',
+    image: 'https://images.reverb.com/image/upload/s--hLgzeFps--/f_auto,t_large/v1557248021/gpxirvi4bggpgmzsbrcs.jpg',   
     value: '4',
     description: 'Looks like its been in the bottom of a lake for 20 years.  Terrible condition.  Not the same bass in the picture.',
     price: 8
 };
 
 const triangle = {
-    id: 'triangle',
+    id: 5,
     name: '2/3rds missing triangle',
     category: 'other',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.long-mcquade.com%2F79466%2FDrums%2FConcert---Marching-Percussion%2FRemo%2FCrown-Percussion-Triangle---Chrome.htm&psig=AOvVaw2mHju1i6Alh-Y9VUf3ZXdL&ust=1586297693039000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjDq8bp1OgCFQAAAAAdAAAAABAD',
+    image: 'https://i.pinimg.com/originals/40/70/f5/4070f5286ba0fff1940fe7026f35d606.jpg',    
     value: '5',
     description: 'Missing two of the three sides.  Just one metal bar.  Not the same triangle in the picture.',
     price: .50
 };
 
 const synth = {
-    id: 'synth',
+    id: 6,
     name: 'Mostly missing synth',
     category: 'keys',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sweetwater.com%2Fstore%2Fdetail%2FJupiterX--roland-jupiter-by-synthesizer&psig=AOvVaw0CMHzQvuv9IeVcUDp9gJYY&ust=1586297721555000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDV6tbp1OgCFQAAAAAdAAAAABAD',
+    image: 'https://cornermusic.com/images/products/49274.jpg',   
     value: '6',
     description: 'Missing all but one key.  Not the same synth as the picture.',
     price: 1
 };
 
 const flute = {
-    id: 'flute',
+    id: 7,
     name: 'Stick',
     category: 'woodwind',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FMerano-Nickel-Silver-Student-Beginners%2Fdp%2FB00CFTRCZK&psig=AOvVaw2nhS1RB_ZS_E0sS_VVr9Qt&ust=1586297826457000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJiw-4bq1OgCFQAAAAAdAAAAABAD',
+    image: 'https://az58332.vo.msecnd.net/e88dd2e9fff747f090c792316c22131c/Images/Products98502-1200x1200-1202834.jpg',   
     value: '7',
     description: 'It\'s a broken piece of driftwood.  Not actually a flute.',
     price: 3
--- renderInstruments.js
+++ renderInstruments.js
@@ -10,13 +10,19 @@ function renderInstruments(instrument) {
     const instrumentDescription = document.createElement('span');
     instrumentDescription.textContent = instrument.description;
     
-    const instrumentCategory = document.createElement('span');
+    const instrumentCategory = document.createElement('h2');
     instrumentCategory.textContent = instrument.category;
 
-    const instrumentPrice = document.createElement('span');
-    instrumentPrice.textContent = instrument.price;
+    // const instrumentPrice = document.createElement('h5');
+    // instrumentPrice.textContent = '$' + instrument.price;
 
-    list.append(h2, image, instrumentDescription, instrumentCategory, instrumentPrice);
+    const button = document.createElement('button');
+    button.textContent = '$' + instrument.price;
+    button.value = instrument.id;
+
+    list.append(h2, image, instrumentDescription, instrumentCategory, button);
+
+
 
     return list;
 };
