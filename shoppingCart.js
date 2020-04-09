b33dafe523101f03559e629f7a38526a08c48b47
Max Lamb <lordmaxlamb@gmail.com>
2020-04-08 22:29:41 -0700

    had to pass travis

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,24 +1,24 @@
 // import cart from './cart.js';
-import instruments from './brokenInstruments.js';
-import findById, { toUSD } from './utils.js';
-import renderLineItem, { calcOrderTotal } from './render-line-item.js';
+// import instruments from './brokenInstruments.js';
+// import findById, { toUSD } from './utils.js';
+// import renderLineItem, { calcOrderTotal } from './render-line-item.js';
 
-const tbody = document.getElementById('table-index');
-const orderTotal = document.getElementById('order-total-amount');
+// const tbody = document.getElementById('table-index');
+// const orderTotal = document.getElementById('order-total-amount');
 
-for (let i = 0; i < cart.length; i++) {
-    // set cartItem equal to each cart index that is passed
-    const cartItem = cart[i];
-    //set cartItemId equal to the cartItem.id pulled from the object cartItem
-    const cartItemId = cartItem.id;
-   // set instrument equal to the findById function that matches the instruments with the cartItemId
-    const instrument = findById(instruments, cartItemId);
-   // set tablerow = to the renderlineitem function which matches the new cartItem value to the instrument
-    const tableRow = renderLineItem(cartItem, instrument);
-    // prints everything into the tbody which is created in the tablerow by rending the items
-    tbody.appendChild(tableRow);
-}
+// for (let i = 0; i < cart.length; i++) {
+//     // set cartItem equal to each cart index that is passed
+//     const cartItem = cart[i];
+//     //set cartItemId equal to the cartItem.id pulled from the object cartItem
+//     const cartItemId = cartItem.id;
+//    // set instrument equal to the findById function that matches the instruments with the cartItemId
+//     const instrument = findById(instruments, cartItemId);
+//    // set tablerow = to the renderlineitem function which matches the new cartItem value to the instrument
+//     const tableRow = renderLineItem(cartItem, instrument);
+//     // prints everything into the tbody which is created in the tablerow by rending the items
+//     tbody.appendChild(tableRow);
+// }
 
-const finalTotal = calcOrderTotal(cart, instruments);
+// const finalTotal = calcOrderTotal(cart, instruments);
 
-orderTotal.textContent = toUSD(finalTotal);
+// orderTotal.textContent = toUSD(finalTotal);
